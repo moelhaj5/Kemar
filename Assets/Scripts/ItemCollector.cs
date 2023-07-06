f1606476cafb08620f392cb7623feb304f7970e5
moelhaj5 <136800293+moelhaj5@users.noreply.github.com>
2023-07-06 16:58:25 +0100

    new version
    Spike deaths

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private int cherries = 0;
+
+    [SerializeField] private Text cherriesText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Cherry")) 
+        {
+
+            Destroy(collision.gameObject);
+
+            cherries++;
+
+            cherriesText.text = "Cherries: " + cherries;
+        }
     }
 }
